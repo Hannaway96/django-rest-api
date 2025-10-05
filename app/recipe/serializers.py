eea930b78fda9f3f8c43cfe028c66f8d25f50dcf
Jack Hannaway <jhannaway@moderndemocracy.com>
2025-10-05 23:00:48 +0100

    Creating and getting tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,15 +3,42 @@ Serializers for Recipe API Views
 """
 
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag
+)
+
+
+class TagSerialzer(serializers.ModelSerializer):
+    """Serializer for Tags"""
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for Recipes"""
+    tags = TagSerialzer(many=True, required=False)
+
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a Recipe"""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
