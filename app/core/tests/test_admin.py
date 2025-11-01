0a76d4d6fc78b42bcc80d283c680be43ea14e0fd
Jack Hannaway <jack_hannaway@icloud.com>
2025-11-01 21:58:17 +0000

    Formatting

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,9 +2,10 @@
 Tests for Djano Admin modifications
 """
 
-from django.test import TestCase, Client as HttpTestClient
-from django.http import HttpResponse
 from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.test import Client as HttpTestClient
+from django.test import TestCase
 from django.urls import reverse
 
 
@@ -15,19 +16,16 @@ class AdminSiteTests(TestCase):
         """Create user and client"""
         self.client: HttpTestClient = HttpTestClient()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@example.com',
-            password="testpass123"
+            email="admin@example.com", password="testpass123"
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email="user@example.com",
-            password="testpass123",
-            name="Test User"
+            email="user@example.com", password="testpass123", name="Test User"
         )
 
     def test_users_list(self):
         """Test that users are listed on page"""
-        url: str = reverse('admin:core_user_changelist')
+        url: str = reverse("admin:core_user_changelist")
         res: HttpResponse = self.client.get(url)
 
         self.assertContains(res, self.user.name)
@@ -35,12 +33,12 @@ class AdminSiteTests(TestCase):
 
     def test_edit_user_page(self):
         """Test the edit user page works"""
-        url = reverse('admin:core_user_change', args=[self.user.id])
+        url = reverse("admin:core_user_change", args=[self.user.id])
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
         """Test the create user page works"""
-        url = reverse('admin:core_user_add')
+        url = reverse("admin:core_user_add")
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
